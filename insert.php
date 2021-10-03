0f280052d9e5c4fcde7f82a5111449ca818c31b4
bukanspot <wicaksana0701@gmail.com>
2021-10-04 01:15:09 +0800

    add insert file

---
+++ insert.php
@@ -0,0 +1,12 @@
+<?php
+     include 'connect.php';
+     $voltage = $_GET['voltage'];
+     $current = $_GET['current'];
+     $power = $_GET['power'];
+     $energy = $_GET['energy'];
+     $frequency = $_GET['frequency'];
+     $pf = $_GET['pf'];
+
+     $sql = "INSERT INTO R (voltage, current, power, energy, frequency, pf) VALUE ('$voltage', '$current', '$power', '$energy', '$frequency', '$pf')";
+     $query = mysqli_query($con, $sql);
+  ?>
