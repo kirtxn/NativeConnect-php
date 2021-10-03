2c7be06630bfdb393777ec20203f39b14dd8b834
bukanspot <wicaksana0701@gmail.com>
2021-10-04 00:05:22 +0800

    make include

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "spot", "W", "monitor");
-
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
+include 'connect.php';
 
 $voltage = $_GET['voltage'];
 $current = $_GET['current'];
