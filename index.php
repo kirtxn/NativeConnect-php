7d51920aa61fcdc5f43e114f1d154b6afa7fdab9
bukanspot <wicaksana0701@gmail.com>
2021-10-03 23:18:41 +0800

    implement get

--- index.php
+++ index.php
@@ -7,4 +7,14 @@ if (mysqli_connect_errno()) {
   exit();
 }
 
+$voltage = $_GET['voltage'];
+$current = $_GET['current'];
+$power = $_GET['power'];
+$energy = $_GET['energy'];
+$frequency = $_GET['frequency'];
+$pf = $_GET['pf'];
+
+$sql = "INSERT INTO R (voltage, current, power, energy, frequency, pf) VALUE ('$voltage', '$current', '$power', '$energy', '$frequency', '$pf')";
+$query = mysqli_query($con, $sql);
+
 ?>
