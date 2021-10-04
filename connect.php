dfac5fd290025b369fb49649a52d690da6796eb2
bukanspot <wicaksana0701@gmail.com>
2021-10-05 00:31:20 +0800

    clear cace for gitignore

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-     $connect = mysqli_connect("localhost", "spot", "W", "monitor");
-
-     // Check connection
-     if (mysqli_connect_errno()) {
-     echo "Failed to connect to MySQL: " . mysqli_connect_error();
-     exit();
-   }
-?>
