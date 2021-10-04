e8e65fbef877db023a2c202f7bf76f32a9c1e34a
bukanspot <wicaksana0701@gmail.com>
2021-10-04 22:28:39 +0800

    clean templating

---
+++ pages/dashboard.php
@@ -0,0 +1,19 @@
+<main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">
+     <?php include "../layout/navbar.php"; ?>
+     
+     <div class="container-fluid py-">
+          <div class="col-lg-12">
+               <div class="card z-index-2">
+                    <div class="card-header pb-0">
+                         <div style="width: 800px;margin: 0px auto;">
+                              <canvas id="myChart"></canvas>
+                         </div>
+                         <p class="text-sm">
+                              <i class="fa fa-arrow-up text-success"></i>
+                              <span class="font-weight-bold">4% more</span> in 2021
+                         </p>
+                    </div>
+               </div>
+          </div>
+     </div>
+</main>
