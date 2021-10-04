a0a9c876925515afb1881777f9cadcb16cff9b9d
bukanspot <wicaksana0701@gmail.com>
2021-10-04 14:38:36 +0800

    add comment

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
      // connect to database
      include 'connect.php';
 
-     // R
+     // get data on R
      $voltageR = $_GET['voltageR'];
      $currentR = $_GET['currentR'];
      $powerR = $_GET['powerR'];
@@ -10,7 +10,7 @@
      $frequencyR = $_GET['frequencyR'];
      $pfR = $_GET['pfR'];
 
-     // S
+     // get data on S
      $voltageS = $_GET['voltageS'];
      $currentS = $_GET['currentS'];
      $powerS = $_GET['powerS'];
@@ -18,7 +18,7 @@
      $frequencyS = $_GET['frequencyS'];
      $pfS = $_GET['pfS'];
 
-     // T
+     // get data on T
      $voltageT = $_GET['voltageT'];
      $currentT = $_GET['currentT'];
      $powerT = $_GET['powerT'];
