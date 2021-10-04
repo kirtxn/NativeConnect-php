3e56b8c34a1d494a00279f3f1efc3cea72a22195
bukanspot <wicaksana0701@gmail.com>
2021-10-05 03:33:49 +0800

    add download pdf

--- dashboard/layout/navbar.php
+++ dashboard/layout/navbar.php
@@ -18,7 +18,7 @@
                </div>
                <ul class="navbar-nav  justify-content-end">
                     <li class="nav-item d-flex align-items-center mx-3">
-                         <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
+                         <a href="#" onclick="window.print();;return false;" class="nav-link text-body font-weight-bold px-0">
                               <i class="fa fa-file-pdf-o me-sm-1"></i>
                               <span class="d-sm-inline d-none">PDF</span>
                          </a>
