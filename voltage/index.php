1e3e8bb1c94987572c960cd5e6fc934453ab17c0
bukanspot <wicaksana0701@gmail.com>
2021-10-06 19:23:44 +0800

    move table to directory

--- voltage/index.php
+++ voltage/index.php
@@ -162,48 +162,9 @@
                                         </div>
                                         <div class="card-body px-0 pt-0 pb-2">
                                              <div class="table-responsive p-0">
-                                                  <table class="table align-items-center justify-content-center mb-0">
-                                                       <thead>
-                                                            <tr>
-                                                                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Parameter</th>
-                                                                 <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Status</th>
-                                                                 <th></th>
-                                                                 <th></th>
-                                                            </tr>
-                                                       </thead>
-                                                       <tbody>
-                                                            <tr>
-                                                                 <td>
-                                                                      <div class="d-flex px-2">
-                                                                           <div>
-                                                                                <img src="../assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm rounded-circle me-2" alt="spotify">
-                                                                           </div>
-                                                                           <div class="my-auto">
-                                                                           <h6 class="mb-0 text-sm">Now Voltage</h6>
-                                                                           </div>
-                                                                      </div>
-                                                                 </td>
-                                                                 <td>
-                                                                      <p class="text-sm font-weight-bold mb-0">100v</p>
-                                                                 </td>
-                                                                 <td class="align-middle text-center">
-                                                                      <div class="d-flex align-items-center justify-content-center">
-                                                                           <span class="me-2 text-xs font-weight-bold">50%</span>
-                                                                           <div>
-                                                                           <div class="progress">
-                                                                                <div class="progress-bar bg-gradient-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="200" style="width: 10%;"></div>
-                                                                           </div>
-                                                                           </div>
-                                                                      </div>
-                                                                 </td>
-                                                                 <td class="align-middle">
-                                                                      <button class="btn btn-link text-secondary mb-0">
-                                                                           <i class="fa fa-ellipsis-v text-xs"></i>
-                                                                      </button>
-                                                                 </td>
-                                                            </tr>
-                                                       </tbody>
-                                                  </table>
+                                                  <?php
+                                                       include "table/R.php";
+                                                  ?>
                                              </div>
                                         </div>
                                    </div>
---
+++ voltage/table/R.php
@@ -0,0 +1,42 @@
+<table class="table align-items-center justify-content-center mb-0">
+     <thead>
+          <tr>
+               <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Parameter</th>
+               <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Status</th>
+               <th></th>
+               <th></th>
+          </tr>
+     </thead>
+     <tbody>
+          <tr>
+               <td>
+                    <div class="d-flex px-2">
+                         <div>
+                              <img src="../assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm rounded-circle me-2" alt="spotify">
+                         </div>
+                         <div class="my-auto">
+                         <h6 class="mb-0 text-sm">Now Voltage</h6>
+                         </div>
+                    </div>
+               </td>
+               <td>
+                    <p class="text-sm font-weight-bold mb-0">100v</p>
+               </td>
+               <td class="align-middle text-center">
+                    <div class="d-flex align-items-center justify-content-center">
+                         <span class="me-2 text-xs font-weight-bold">50%</span>
+                         <div>
+                         <div class="progress">
+                              <div class="progress-bar bg-gradient-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="200" style="width: 10%;"></div>
+                         </div>
+                         </div>
+                    </div>
+               </td>
+               <td class="align-middle">
+                    <button class="btn btn-link text-secondary mb-0">
+                         <i class="fa fa-ellipsis-v text-xs"></i>
+                    </button>
+               </td>
+          </tr>
+     </tbody>
+</table>
