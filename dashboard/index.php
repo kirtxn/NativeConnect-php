9c42cc73691545154c6de3d3bf6af352b70541c9
bukanspot <wicaksana0701@gmail.com>
2021-10-06 15:40:55 +0800

    cleanup index

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
      <meta charset="utf-8" />
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,18 +9,18 @@
           Connect
      </title>
 
-     <!--     Fonts and icons     -->
+     <!-- Fonts and icons -->
      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+     
      <!-- Nucleo Icons -->
-
      <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
      <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+     
      <!-- Font Awesome Icons -->
-
      <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
      <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+     
      <!-- CSS Files -->
-
      <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
 </head>
 
@@ -31,11 +30,9 @@
      ?>
      <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">
           <?php
-          include "layout/navbar.php";
-     ?>
-     <?php
-          include '../connect.php';
-     ?>
+               include "layout/navbar.php";
+               include '../connect.php';
+          ?>
           <div class="container-fluid py-4">
                <div class="row mt-4">
                     <div class="col-lg-12">
@@ -151,18 +148,19 @@
      ?>
 
      <script>
-     var win = navigator.platform.indexOf('Win') > -1;
-     if (win && document.querySelector('#sidenav-scrollbar')) {
-          var options = {
-          damping: '0.5'
+          var win = navigator.platform.indexOf('Win') > -1;
+          if (win && document.querySelector('#sidenav-scrollbar')) {
+               var options = {
+               damping: '0.5'
+               }
+               Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
           }
-          Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-     }
      </script>
+
      <!-- Github buttons -->
      <script async defer src="https://buttons.github.io/buttons.js"></script>
+     
      <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
      <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
 </body>
-
 </html>
